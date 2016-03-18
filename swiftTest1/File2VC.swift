65b9d8747f39086aea0a284069b6c3bb2d302a3a
maple <zai@mapledeMacBook-Pro.local>
2016-03-18 14:12:15 +0800

    添加一个控制器

---
+++ swiftTest1/File2VC.swift
@@ -0,0 +1,13 @@
+//
+//  File2VC.swift
+//  swiftTest1
+//
+//  Created by maple on 16/3/18.
+//  Copyright © 2016年 zaiwei. All rights reserved.
+//
+
+import UIKit
+
+class File2VC: UIViewController {
+    
+}
